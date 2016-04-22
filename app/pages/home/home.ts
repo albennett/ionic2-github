7e8ce4062fff8fb128f49d3e9983a630fc90a87f
Amy Lee <amyleeannebennett@gmail.com>
2016-04-22 15:22:37 -0500

    search for repos of username with github api

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,24 @@
 import {Page} from 'ionic-angular';
+import {GitHubService} from '../../services/github';
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html',
+    providers: [GitHubService]
 })
 export class HomePage {
-  constructor() {
+    public foundRepos;
+    public username;
 
-  }
+    constructor(private github: GitHubService) {
+    }
+
+    getRepos() {
+        this.github.getRepos(this.username).subscribe(
+            data => {
+                this.foundRepos = data.json();
+            },
+            err => console.error(err),
+            () => console.log('getRepos completed')
+        );
+    }
 }
